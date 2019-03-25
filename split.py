cfcd313333aa25e27a5b077e6a1374bf1298542c
Tatsuya Takaji <42364759+t-takaji@users.noreply.github.com>
2019-03-26 01:26:59 +0900

    可読性UP

--- split.py
+++ split.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
 
-in_file_name = "t_GoodsVisitor20190307_171507.csv"
-out_file_name_template = "split_%d.csv"
+'''
+処理概要
+whileで入力ファイルを順に読み込み、1行ずつ出力ファイルに書き出す。
+指定した行まで到達すると出力ファイルを閉じ、採番して次の出力ファイルに書き込む
+〜以降繰り返し〜
+'''
 
-max_lines = 10000
+# ファイル名は任意だが、出力ファイルに関しては、%dを含むこと
+# ※採番して出力するため
+input_file_name = "hoge.csv"
+output_file_name_template = "split_%d.csv"
 
-split_index = 1
-line_index = 1
-out_file = open(out_file_name_template % split_index, "w")
-in_file = open(in_file_name)
-line = in_file.readline()
+# １ファイルあたりの行数
+max_lines = 1000
+
+# 初期処理
+line_no = 1
+numbering = 1
+output_file = open(output_file_name_template % numbering, "w")
+input_file = open(input_file_name)
+line = input_file.readline()
+
 while line:
-    if line_index > max_lines:
-        out_file.close()
-        split_index = split_index + 1
-        line_index = 1
-        out_file = open(out_file_name_template % split_index, "w")
-    out_file.write(line)
-    line_index = line_index + 1
-    line = in_file.readline()
+    if line_no > max_lines:
+        output_file.close()
+        numbering += 1
+        line_no = 1
+        output_file = open(output_file_name_template % numbering, "w")
+    output_file.write(line)
+    line_no += 1
+    line = input_file.readline()
 
-out_file.close()
-in_file.close()
+output_file.close()
+input_file.close()
