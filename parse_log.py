753e0af3e687fd2528ec60915acac9a5ccccf6c2
Tatsuya Takaji <42364759+t-takaji@users.noreply.github.com>
2021-01-27 17:58:52 +0900

    log解析用

---
+++ parse_log.py
@@ -0,0 +1,17 @@
+import sys, re
+
+# 入出力のパス
+input_path = './test.log'
+output_path = './result.txt'
+
+# Grep検索対象
+grep_target = '2021-01-27 00:00'
+
+# ファイル読み込みとGrep
+with open(input_path, mode='r', encoding='utf-8') as f:
+    lines = f.readlines()
+    GREP_TARGET = [line for line in lines if grep_target in line]
+
+# ファイル出力
+with open(output_path, mode='w', encoding='utf-8') as f:
+    f.writelines(GREP_TARGET)
