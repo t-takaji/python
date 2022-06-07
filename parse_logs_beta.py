b05e017c524d15be0a9ff9dd3bdde16dd4cf5e2a
t-takaji <42364759+t-takaji@users.noreply.github.com>
2022-06-07 13:04:49 +0900

    Update parse_logs_beta.py

--- parse_logs_beta.py
+++ parse_logs_beta.py
@@ -6,38 +6,44 @@
 '''
 import os
 
-## 設定情報
-######################################################################
-# ファイルパス
-input_directory_path = "C:/test"
-output_path = "C:/result.txt"
+# ログ配置ディレクトリ
+input_directory_path = os.getcwd().replace(os.sep, '/') + "/log"
+# 出力ファイル
+output_file = os.getcwd().replace(os.sep, '/') + "/result.txt"
 # 検索対象
-grep_target1 = "EXCEPTION"
-######################################################################
-
-os.chdir(input_directory_path)
+grep_target = "exception"
 
 
-def read_text_file(file_path):
+def init():
+    # ディレクトリ移動
+    os.chdir(input_directory_path)
+
+    # 出力ファイル初期化
+    is_file = os.path.isfile(output_file)
+    if is_file:
+        os.remove(output_file)
+
+def read_files(file_path):
     with open(file_path, 'r', encoding="utf-8") as f:
         lines = f.readlines()
         # 指定した文字列を含む行を取得
-        GREP_TARGET = [line for line in lines if grep_target1 in line]
-        # orで条件追加できる
-        # GREP_TARGET = [line for line in lines if grep_target1 in line or grep_target2 in line]
+        GREP_TARGET = [line for line in lines if grep_target in line]
+        # ターゲットのセッションIDをリスト化
         SESSION_ID_LIST = [item.split()[3] for item in GREP_TARGET]
 
     # 重複行削除
-    result = list(set(SESSION_ID_LIST))
+    session_id_list = list(set(SESSION_ID_LIST))
+
     with open(file_path, 'r', encoding="utf-8") as f:
         lines = f.readlines()
         # 指定した文字列を含む行を取得
-        ACTION = [line for line in lines for a in result if a in line]
+        ACTION = [line for line in lines for s in session_id_list if s in line]
 
-    with open(output_path, mode='w', encoding='utf-8') as f:
+    with open(output_file, mode='a', encoding='utf-8') as f:
         f.writelines(ACTION)
 
+init()
 
 for file in os.listdir():
     file_path = f"{input_directory_path}\{file}"
-    read_text_file(file_path)
+    read_files(file_path)
